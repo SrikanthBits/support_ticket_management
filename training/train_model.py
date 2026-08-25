32ecc7dfbbc18a02b52abecf24272370391bfdac
SrikanthBits <2025paml599@wilp.bits-pilani.ac.in>
2026-08-26 00:35:18 +0530

    Feature store by using sqlite

--- training/train_model.py
+++ training/train_model.py
@@ -15,3 +15,42 @@ import sqlite3
 # Load dataset
 print("SCRIPT STARTED") # <-- Add this line
 print("Does file exist?:", os.path.exists('data/customer_support_tickets.csv'))
+
+# -------------------------------
+# M2: Data Engineering & Validation
+# -------------------------------
+
+def clean_text(text):
+    text = str(text).lower()
+    text = re.sub(r"http\S+", "", text)
+    text = re.sub(r"@\w+", "", text)
+    text = re.sub(r"#\w+", "", text)
+    text = text.translate(str.maketrans("", "", string.punctuation))
+    return text.strip() 
+
+df = pd.read_csv('data/customer_support_tickets.csv')
+print("Dataset loaded successfully.", df.head())
+
+# Features stored in SQLite database
+
+db_path = os.path.abspath("data/customer_support_tickets_features.db")
+
+# Feature store - Save to SQLite using sqlite3
+conn = sqlite3.connect(db_path)
+df.to_sql("customer_support_tickets", conn, if_exists="replace", index=False)
+conn.close()
+
+# engine = create_engine("sqlite:///data/customer_support_tickets_features.db")
+# df.to_sql("customer_support_tickets", engine, if_exists="replace", index=False)
+
+print("✅ Table created from CSV headers")
+
+
+connection = sqlite3.connect('data/customer_support_tickets_features.db')
+print(connection.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchall())
+features = pd.read_sql_query("SELECT * FROM customer_support_tickets", connection)
+connection.close()
+
+print("Rows in SQLite database:", len(features))
+print("Columns in SQLite database:", features.columns)
+print("Data loaded from SQLite database successfully.", features.head())
